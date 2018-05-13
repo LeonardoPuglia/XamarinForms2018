a3e0818f548668888673799fd4235568f022e0a2
Leonardo Puglia <leo.puglia@hotmail.com>
2018-05-13 11:56:20 -0300

    Finalizando o estudo de layouts

---
+++ Seção 07/App3_LayoutXF/App3_LayoutXF/App3_LayoutXF/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App3_LayoutXF
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        #region Button Events
+
+        private void GoAbsolutePage(object sender , EventArgs args)
+        {
+            Navigation.PushAsync(new Layouts.Absolute.AbsolutePage());
+        }
+
+        private void GoGridPage(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Layouts.Grid.GridPage());
+        }
+
+        private void GoRelativePage(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Layouts.Relative.RelativePage());
+        }
+
+        private void GoScrollPage(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Layouts.Scroll.ScrollPage());
+        }
+
+        private void GoStackPage(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Layouts.Stack.StackPage());
+        }
+
+
+        #endregion
+    }
+}
